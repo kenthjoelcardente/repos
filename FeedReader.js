e2068917f783d07605c30665cc9a80b283048ed0
Kenth Joel Cardente <70134844+kenthjoelcardente@users.noreply.github.com>
2022-06-08 16:45:52 +0800

    commit

--- FeedReader.js
+++ FeedReader.js
@@ -13,16 +13,85 @@ userFeedURLs.forEach(userUrl => {
                 var content = document.getElementById('content');
 
                 var newItem = "";
-                newItem += "<div class=\"container\" id=\"item\"><a href=\"" + item.link + "\"><h2>" + item.title + "</h2></a>" + "<h4> from " + data.feed.title + "</h4>";
+                newItem += "<div class=\"container\" id=\"item\"><a href=\"" + item.link + "\"><h4>" + item.title + "</h4></a>" + "<h5> from " + data.feed.title + "</h5>";
                 if (item.author != "")
                     newItem += "<h5> By " + item.author + "</h5>";
 
-                newItem += "<h4>Published Date: " + item.pubDate + "</h4>";
-
-                newItem += item.description + "<hr></div>";
+                newItem += "<h9>Published Date: " + item.pubDate + "</h9>";
 
                 content.insertAdjacentHTML('beforeend', newItem);
             });
         }
     });
 });
+
+var responsiveSlider = function () {
+
+    var slider = document.getElementById("slider");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("slideWrap");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
+
+    window.addEventListener('resize', function () {
+        sliderWidth = slider.offsetWidth;
+    });
+}
+
+var responsiveSlider = function () {
+
+    var slider = document.getElementById("slider");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("slideWrap");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
+
+    window.addEventListener('resize', function () {
+        sliderWidth = slider.offsetWidth;
+    });
+
+    var prevSlide = function () {
+        if (count > 1) {
+            count = count - 2;
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        }
+        else if (count = 1) {
+            count = items - 1;
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        }
+    };
+
+    var nextSlide = function () {
+        if (count < items) {
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        }
+        else if (count = items) {
+            slideList.style.left = "0px";
+            count = 1;
+        }
+    };
+
+    next.addEventListener("click", function () {
+        nextSlide();
+    });
+
+    prev.addEventListener("click", function () {
+        prevSlide();
+    });
+
+    setInterval(function () {
+        nextSlide()
+    }, 5000);
+
+};
+
+window.onload = function () {
+    responsiveSlider();
+}
